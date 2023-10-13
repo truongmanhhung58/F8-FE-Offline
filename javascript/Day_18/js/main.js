01cb49e606941a36684d95f72e7b1df6509a3bd8
Your Name <you@example.com>
2023-10-13 14:52:03 +0700

    nop bai tap ngay 33

---
+++ javascript/Day_18/js/main.js
@@ -0,0 +1,37 @@
+var counter = document.querySelector(".counter");
+var btnGetLink = document.querySelector(".btn-get-link");
+
+var counterTime = 11;
+var checkTime = 0;
+var focusOut = false;
+
+
+function startDown(currentTime) {
+  if (!checkTime) checkTime = currentTime;
+  if (currentTime - checkTime > 1000) {
+    counterTime--;
+    checkTime = currentTime;
+    counter.innerText = counterTime;
+  }
+  if(counterTime > 0) {
+    requestAnimationFrame(startDown);
+  } else {
+    cancelAnimationFrame(startDown)
+    btnGetLink.disabled = false;
+    btnGetLink.addEventListener("click",() => {
+        window.location.href="https://fullstack.edu.vn/courses"
+    })
+  }
+}
+
+startDown();
+
+
+
+
+
+
+
+
+
+
